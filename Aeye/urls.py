66b0fb10d8c60cc01f5ff2f092a98f28d059a579
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-05-06 14:50:31 +0300

    create Products URLs

--- Aeye/urls.py
+++ Aeye/urls.py
@@ -20,6 +20,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('customers/', include("customers.urls"))
+    path('customers/', include("customers.urls")),
+    path('products/', include("products.urls")),
 
 ]
