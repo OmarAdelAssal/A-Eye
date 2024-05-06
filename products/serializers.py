0ca0aa657389f513329655b972e45cfd8f381c28
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-05-06 14:44:03 +0300

    create CategorySerializer and ProductSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,3 +1,16 @@
 from rest_framework import serializers
 
-# Serializer
+from .models import Category, Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'categoryName']
+
+class ProductSerializer(serializers.ModelSerializer):
+    # Nested serializer to include category details
+    category = CategorySerializer()  
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'quanitiy', 'price', 'category']
