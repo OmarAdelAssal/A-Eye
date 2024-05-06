46c87512bb5729dfee3d45161eb1f456e4ba5ba1
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-05-06 15:52:13 +0300

    done

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CategoryViewSet, ProductViewSet
+from .views import ProductViewSet
 
 router = routers.DefaultRouter()
 router.register(r'', ProductViewSet)
