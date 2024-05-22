6e011469c441b947ddced86c33e4ed18d03babf2
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-05-23 00:19:13 +0300

    changes in customer views and urls

--- customers/urls.py
+++ customers/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path, include
-from customers.views import SignUpView, LoginView, LogoutView
+from customers.views import SignUpView, LoginView, LogoutView, CustomerView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'', SignUpView)
 
 router2 = DefaultRouter()
-# router2.register('', LoginView)
+# router2.register('', CustomerViewSet)
 
 urlpatterns = [
     path('signup/', include(router.urls)),
     # path("login/", LoginView.as_view({"post": "login"})),
     # path('login/', include(router2.urls)),
     path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout")
-
+    path("logout/", LogoutView.as_view(), name="logout"),
+    # path("<int:pk>/", CustomerViewSetas_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
+    # path("<int:pk>/",include(router2.urls))
+    path("customer/", CustomerView.as_view())
 ]
 
 
--- customers/views.py
+++ customers/views.py
@@ -64,7 +64,54 @@ class LoginView(APIView):
 
         #if password correct
         return response
-    
+'''
+
+# Create retrive , update , delete on Customer
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+    # Retrieve Customer by id
+    def retrieve(self, request, pk):
+            
+        customer = self.get_queryset().filter(id=pk).first()
+        serializer = self.serializer_class(customer)
+        return Response(serializer.data)
+        # Update Customer Data    
+    def update(self, request, pk):
+
+        customer = self.get_queryset().filter(id=pk).first()
+        serializer = self.serializer_class(customer, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # Delete Customer
+    def destroy(self, request, pk):
+        customer = self.get_queryset().filter(id=pk).first()
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+# get customer using cookie
+class CustomerView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed("Unauthenticated!")
+        
+        try:
+            payload = jwt.decode(token, 'secret', algorithms="HS256")
+            #decode gets the Customer
+
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Unauthenticated!")
+        
+        customer = Customer.objects.filter(id=payload['id']).first()
+        serializer = CustomerSerializer(customer)
+
+        return Response(serializer.data)
+        #cookies accessed if preserved
+
 
 class LogoutView(APIView):
     def post(self, request):
