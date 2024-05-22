58a4727103207606b188f010b868442fd952a73d
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-05-22 12:42:28 +0300

    Great Changes

--- customers/urls.py
+++ customers/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path, include
-from customers.views import SignUpView
+from customers.views import SignUpView, LoginView, LogoutView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'', SignUpView)
 
-
+router2 = DefaultRouter()
+# router2.register('', LoginView)
 
 urlpatterns = [
     path('signup/', include(router.urls)),
+    # path("login/", LoginView.as_view({"post": "login"})),
+    # path('login/', include(router2.urls)),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout")
 
 ]
 
--- customers/views.py
+++ customers/views.py
@@ -5,6 +5,11 @@ from rest_framework import status
 from .models import Customer
 from .serializers import CustomerSerializer
 
+import jwt, datetime
+from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
+
+
 class SignUpView(viewsets.ModelViewSet):
     """
     Create a new Customer.
@@ -20,13 +25,53 @@ class SignUpView(viewsets.ModelViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# class SignUpView(viewsets.ModelViewSet):
-#     queryset = Customer.objects.all()
-#     serializer_class = CustomerSerializer
 
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_create(serializer)
-#         headers = self.get_success_headers(serializer.data)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        #find Customer using email
+        customer = Customer.objects.filter(email=email).first()
+
+        if customer is None:
+            raise AuthenticationFailed('Customer not found:)')
+            
+        if not customer.check_password(password):
+            raise AuthenticationFailed('Invalid password')
+
+        payload = {
+            "id": customer.id,
+            "email": customer.email,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            "iat": datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+        # token.decode('utf-8')
+        #we set token via cookies
+        
+
+        response = Response() 
+
+        response.set_cookie(key='jwt', value=token, httponly=True)  #httonly - frontend can't access cookie, only for backend
+
+        response.data = {
+            'jwt token': token
+        }
+
+        #if password correct
+        return response
+    
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'successful'
+        }
+
+        return response
