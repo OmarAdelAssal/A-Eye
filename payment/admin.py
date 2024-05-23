690bf0766f1c558577719d598f4ce4b81966cf4d
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-05-23 22:37:19 +0300

    done

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'payment_method', 'payment_date')
+    search_fields = ('customer__name', 'payment_method')
+    list_filter = ('payment_method', 'payment_date')
+    ordering = ('-payment_date',)
+
+admin.site.register(Payment, PaymentAdmin)
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,19 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+from .models import Customer
+
+class Payment(models.Model):
+
+    PAYMENT_METHOD_CHOICES = (
+        ('Credit Card', 'Credit Card'),
+        ('Cash on Delivery ', 'Cash on Delivery'),
+
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=50, choices=PAYMENT_METHOD_CHOICES)
+    payment_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Payment {self.id} - {self.customer.name}"
