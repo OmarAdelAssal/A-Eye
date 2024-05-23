b59c59262eff1f864f79ea417c092b78a8b61ea7
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-05-23 16:05:16 +0300

    done

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import ProductViewSet
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'', ProductViewSet)
@@ -11,4 +13,4 @@ urlpatterns = [
     path('<int:pk>/', ProductViewSet.as_view({'get': 'retrieve_product'}), name='product-detail'),
     
     
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
