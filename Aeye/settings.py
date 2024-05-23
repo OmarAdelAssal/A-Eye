eac0a43456c88e5594fa227f6e736e47e7d58e2e
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-05-23 11:46:26 +0300

    Push Media files and edit media in Aeye settings

--- Aeye/settings.py
+++ Aeye/settings.py
@@ -137,3 +137,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = "/media/"
+
+import os
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
