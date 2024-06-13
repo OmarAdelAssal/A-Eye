cd556d61a13c237515a6adbbb4715eb05f2ba452
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-06-13 12:36:02 +0300

    add staticfiles for styling admin panel and all rest pages

--- Aeye/settings.py
+++ Aeye/settings.py
@@ -139,9 +139,9 @@ AUTH_USER_MODEL = 'customers.Customer'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
