d825617c68dd94a1d208fce8edd649b055a86bf2
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-06-20 03:33:04 +0300

    add order admin to dashboard

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-# from .models import Order
+from .models import Order, OrderItem
 
-# admin.site.register(Order)
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1  # Number of empty inline forms to display
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'created_at', 'total_cost')
+    list_filter = ('created_at', 'customer')
+    search_fields = ('customer__email', 'id')
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'price')
+    list_filter = ('order', 'product')
+    search_fields = ('order__id', 'product__name')
