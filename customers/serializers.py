97616dbae37aa470682ff660b832bbb5f08e187a
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2024-06-21 14:22:55 +0300

    password validation

--- customers/serializers.py
+++ customers/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Customer
 
 class CustomerSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=5, required=True)
     # serializer for Customer model
     class Meta:
         model = Customer
